c832f9c8a3bb6e6b537f0be121735d4ae7218c7f
Emiliano Sotomayor Gonzalez <uboat46@hotmail.com>
2016-09-21 21:26:18 -0500

    Initial commit

---
+++ app/src/main/java/com/example/uboat46/prueba1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.uboat46.prueba1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
